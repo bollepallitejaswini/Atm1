fa66790b1860ba54345ea9cec1a4f967d3bf964f
hp <hp@DESKTOP-3K50UAU>
2023-07-20 10:42:50 +0530

    Add project files.

---
+++ Atm1/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public partial class partial
+    {
+        private int _accno;
+        private string _name;
+        private string _atm;
+        private int _amt;
+        private int _age;
+        private int _camt;
+        private int _wamt;
+        private string _with;
+        public int accno
+        {
+            set { _accno = value; }
+            get { return _accno; }
+        }
+        public string name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public string atm
+        {
+            get { return _atm; }
+            set { _atm = value; }
+        }
+        public int amt
+        { get { return _amt; } set { _amt = value; } }
+
+
+        public int age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+        public int camt
+        { get { return _camt; } set { _camt = value; } }
+        public string with
+        {
+            get { return _with; }
+            set { _with = value; }
+        }
+        public int wamt
+        {
+            get { return _wamt; }
+            set { _wamt = value; }
+        }
+    }
+}
---
+++ Atm1/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class AException : Exception
+    {
+        public AException(int cmat)
+        {
+            cmat = cmat - 500;
+            Console.WriteLine("");
+            Console.WriteLine("Rs 500 deducted from your account as limit of no. of transactions is exceeded\n ");
+            Console.Write("Total Amount Available: " + cmat);
+            Console.WriteLine("");
+        }
+
+    }
+    class Program
+    {
+        public static void withd(string atm, string with, int camt)
+        {
+            char wa;
+            int count = 0;
+            if ((atm == "y") && (with == "y"))
+            {
+
+
+                //for (int i = 0; i < 3; i++)
+                //{
+                do
+                {
+                    Console.Write("Enter transaction amount: ");
+                    count++;
+                    int n = Convert.ToInt32(Console.ReadLine());
+                    camt = camt - (int)n;
+                    Console.WriteLine("Total amount after transaction: " + camt);
+                    if (count == 3)
+                    {
+                        throw new AException(camt);
+                    }
+                    Console.Write("do you want to withdraw again(y/n):");
+                    wa = Convert.ToChar(Console.ReadLine());
+                } while (wa == 'y');
+
+                //}  
+
+                Console.WriteLine("Total amount available after transaction: " + camt);
+            }
+            else
+            {
+                Console.WriteLine("Your accounts are created and details are saved ");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            partial p = new ConsoleApp1.partial();
+            Console.WriteLine("enter savings accno,name:");
+            p.accno = int.Parse(Console.ReadLine());
+            p.name = Console.ReadLine();
+            Console.WriteLine("do you need atm card(y/n):");
+            p.atm = Console.ReadLine();
+            Console.Write("Enter Initial deposit amount:");
+            p.camt = int.Parse(Console.ReadLine());
+            Console.Write("do you want to withdraw money form our account(y/n):");
+            p.with = Console.ReadLine();
+            Program.withd(p.atm, p.with, p.camt);
+
+            Console.WriteLine("enter current accno,name:");
+            p.accno = int.Parse(Console.ReadLine());
+            p.name = Console.ReadLine();
+            Console.WriteLine("do you need atm card(y/n):");
+            p.atm = Console.ReadLine();
+            Console.Write("Enter Initial deposit amount:");
+            p.camt = int.Parse(Console.ReadLine());
+            Console.Write("do you want to withdraw money form our account(y/n):");
+            p.with = Console.ReadLine();
+            Program.withd(p.atm, p.with, p.camt);
+
+            Console.WriteLine("enter child care accno,name and age:");
+            p.accno = int.Parse(Console.ReadLine());
+            p.name = Console.ReadLine();
+            p.age = int.Parse(Console.ReadLine());
+            Console.WriteLine("do you need atm card(y/n):");
+            p.atm = Console.ReadLine();
+            Console.Write("Enter Initial deposit amount:");
+            p.camt = int.Parse(Console.ReadLine());
+            Console.Write("do you want to withdraw money form our account(y/n):");
+            p.with = Console.ReadLine();
+            Program.withd(p.atm, p.with, p.camt);
+
+        }
+    }
+}
+
